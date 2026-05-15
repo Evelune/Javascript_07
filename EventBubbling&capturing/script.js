515111335e4d53b507cd63dd2e8f1141ea3983e6
singhdewang007 <singh.dewang.01@gmail.com>
2026-05-15 22:26:28 +0530

    eventBubbling

---
+++ EventBubbling&capturing/script.js
@@ -0,0 +1,23 @@
+document.querySelector("#grandparent").addEventListener(
+  "click",
+  () => {
+    console.log("GrandParent Clicked");
+  },
+  true,
+);
+
+document.querySelector("#parent").addEventListener(
+  "click",
+  () => {
+    console.log("Parent Clicked");
+  },
+  true,
+);
+
+document.querySelector("#child").addEventListener(
+  "click",
+  () => {
+    console.log("child Clicked");
+  },
+  true,
+);
